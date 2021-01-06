283dd6b9484b974256bfd33184bcb7327e2c67ae
island47 <island0318@gmail.com>
2021-01-07 04:19:41 +0900

    carrot #1

---
+++ carrot/src/main.js
@@ -0,0 +1,134 @@
+'use strict';
+import PopUp from './popup.js';
+import Field from './field.js';
+import * as sound from './sound.js';
+
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
+
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+let started = false;
+let timer = undefined;
+let score = 0;
+
+const gameFinishBanner = new PopUp();
+gameFinishBanner.setClickListener(() => {
+	startGame();
+});
+
+const gameField = new Field(CARROT_COUNT, BUG_COUNT);
+gameField.setClickListener(onItemClick);
+
+function onItemClick(item) {
+	if (!started) {
+		return;
+	}
+	if (item === 'carrot') {
+		score++;
+		updateScoreBoard();
+		if (score === CARROT_COUNT) {
+			finishGame(true);
+		}
+	} else if (item === 'bug') {
+		finishGame(false);
+	}
+}
+
+// 사운드
+
+// 버튼
+gameBtn.addEventListener('click', () => {
+	if (started) {
+		stopGame();
+	} else {
+		startGame();
+	}
+});
+
+function startGame() {
+	started = true;
+	initGame();
+	showStopButton();
+	showTimerAndScore();
+	startGameTimer();
+	sound.playBackGround();
+}
+
+function stopGame() {
+	started = false;
+	stopGameTimer();
+	hideGameButton();
+	gameFinishBanner.showWithText('REPLAY ❓');
+	sound.playAlert();
+	sound.stopBackGround();
+}
+
+function finishGame(win) {
+	started = false;
+	hideGameButton();
+	if (win) {
+		sound.playWin();
+	} else {
+		sound.playBug();
+	}
+	stopGameTimer();
+	sound.stopBackGround();
+	gameFinishBanner.showWithText(win ? 'YOU WON ✨' : 'YOU LOST 💩');
+}
+
+function showStopButton() {
+	const icon = gameBtn.querySelector('.fas');
+	icon.classList.add('fa-stop');
+	icon.classList.remove('fa-play');
+	gameBtn.style.visibility = 'visible';
+}
+
+function showTimerAndScore() {
+	gameTimer.style.visibility = 'visible';
+	gameScore.style.visibility = 'visible';
+}
+
+function hideGameButton() {
+	gameBtn.style.visibility = 'hidden';
+}
+
+function startGameTimer() {
+	let remainingTimeSec = GAME_DURATION_SEC;
+	updateTimerText(remainingTimeSec);
+	timer = setInterval(() => {
+		if (remainingTimeSec <= 0) {
+			clearInterval(timer);
+			finishGame(CARROT_COUNT === score);
+			return;
+		}
+		updateTimerText(--remainingTimeSec);
+	}, 1000);
+}
+
+function stopGameTimer() {
+	clearInterval(timer);
+	hideGameButton();
+	gameFinishBanner.showWithText('REPLAY ❓');
+}
+
+function updateTimerText(time) {
+	const minutes = Math.floor(time / 60);
+	const seconds = time % 60;
+	gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function initGame() {
+	score = 0;
+	gameScore.innerText = CARROT_COUNT;
+	gameField.init();
+}
+
+// 필드 클릭 이벤트
+
+function updateScoreBoard() {
+	gameScore.innerText = CARROT_COUNT - score;
+}
